04b7aac661ac48347030fe67355374b1b3fbe82d
coin-123 <isobednlemchukwu@gmail.com>
2025-09-12 20:26:46 +0100

    Added to mobile view

--- src/components/course/course.jsx
+++ src/components/course/course.jsx
@@ -6,34 +6,80 @@ import frame4 from '../../assets/image14.png'
 import frame5 from '../../assets/image15.png'
 import { motion } from 'framer-motion'
 
+    const items = [
+        {
+              title: "Frontend Development",
+              sub: "12 Weeks",
+              description:
+                "Fast, responsive, and user-friendly interfaces tailored to your needs.",
+              image: frame1,
+            },
+        {
+              title: "Backend Development",
+              sub: "12 Weeks",
+                description:
+                "Secure, scalable, and high perfomance solutions.",
+              image: frame2,
+            },
+        {
+              title: "UI/UX Design",
+              sub: "12 Weeks",
+              description:
+              "User-friendly and visually stunning designs that keep users engaged.",
+              image: frame3,
+        },
+
+        {
+            title:"Cyber-Security",
+            Sub:"12 weeks",
+            description:
+            "Protect data, systems, and networks with top-notch cybersecurity skill.",
+            image:frame4,
+        },
+
+        {
+            title:"Data analysis",
+            sub:"12 weeks",
+            description:
+            "Turn raw data into actionable insights with advanced analytics.",
+            image:frame5,
+        }
+
+    ];
+
 
 
 const Course = () => {
+
   return (
     <motion.section
     initial={{ opacity: 0, y: 50 }}        // start hidden & pushed down
   whileInView={{ opacity: 1, y: 0 }}     // animate when in view
   transition={{ duration: 0.8, ease: "easeOut" }}
   viewport={{ once: true, amount: 0.2 }}
-     className="flex items-center justify-center w-full h-[1256px] bg-[rgba(237,255,226,1)]" id='courses'>
+     className="flex items-center justify-center w-full bg-[rgba(237,255,226,1)]" id='courses'>
         <div className="flex flex-col items-center justify-around w-full h-full ">
-            <div className="flex flex-col items-center justify-center w-[550px] b-pink-600">
-                <h1 className="text-[3.2rem] font-semibold text-center  ">Our coureses</h1>
-                <p className="text-[1.5rem] text-center">Well tailored and certified courses designed to help you unlock your full career potentials.</p>
+            <div className="flex flex-col items-center justify-center w-full b-pink-600 mb-[3rem] sm:mb-[3rem]]">
+                <h1 className="text-[2rem] sm:text-[3.2rem] font-semibold text-center  ">Our coureses</h1>
+                <p className="sm:text-[1.5rem] text-[0.7rem] w-[80%] sm:w-[60%] text-center">Well tailored and certified courses designed to help you unlock your full career potentials.</p>
             </div>
         <div className="flex flex-wrap items-center justify-center h-[78%] w-full gap-10 "> 
-            <div className="flex flex-col items-center justify-between rounded-[20px] w-[400px] h-[454px] bg-[rgba(255,255,255,1)] pb-[12px]">
+            {items.map((item, index) => ( 
+            <div
+            key={index} 
+            className="flex flex-col items-center justify-between rounded-[20px] w-[80%] sm:w-[400px] h-[454px] bg-[rgba(255,255,255,1)] pb-[12px]">
                 <div className="flex flex-col items-center justify-center w-full h-[335px]">
-                    <div className="flex items-center justify-center w-full h-[50%]"> <img src={frame1} alt="" className="w-full h-full" /> </div>
+                    <div className="flex items-center justify-center w-full h-[50%]"> <img src={item.image} alt="" className="w-full h-full" /> </div>
                 <div className="flex flex-col items-center justify-center w-full h-[50%]">
-                    <h1 className='font-bold text-[22px] '>Frontend Development</h1>
-                    <p className="text-[22px]">12 Weeks</p>
-                    <p className="text-[22px] text-center  w-[70%]"> Fast, responsive, and user-friendly interfaces tailored to your needs.</p>
+                    <h1 className='font-bold text-[22px] '>{item.title}</h1>
+                    <p className="text-[22px]">{item.sub}</p>
+                    <p className=" text-center  w-[70%] sm:text-[1.5rem] 562428"> {item.description}</p>
                 </div>
                 </div>
                 <button className="bg-[rgba(81,177,70,1)] w-[264px] h-[52px] p-[12px] rounded-lg text-white transition duration-300 hover:bg-white hover:text-[rgba(81,177,70,1)] border-[2px] border-[rgba(81,177,70,1)]">Resgister</button>
             </div>
-             <div className="flex flex-col items-center justify-between rounded-[20px] w-[400px] h-[454px] bg-[rgba(255,255,255,1)] pb-[12px]">
+            ))}
+             {/* <div className="flex flex-col items-center justify-between rounded-[20px] w-[400px] h-[454px] bg-[rgba(255,255,255,1)] pb-[12px]">
                 <div className="flex flex-col items-center justify-center w-full h-[335px]">
                     <div className="flex items-center justify-center w-full h-[50%]"> <img src={frame2} alt="" className="w-full h-full" /> </div>
                 <div className="flex flex-col items-center justify-center w-full h-[50%]">
@@ -76,7 +122,7 @@ const Course = () => {
                 </div>
                 </div>
                 <button className="bg-[rgba(81,177,70,1)] w-[264px] h-[52px] p-[12px] rounded-lg text-white transition duration-300 hover:bg-white hover:text-[rgba(81,177,70,1)] border-[2px] border-[rgba(81,177,70,1)]">Resgister</button>
-            </div>
+            </div> */}
         </div>
         </div>
     </motion.section>
--- src/components/whystrhub/whystrhub.jsx
+++ src/components/whystrhub/whystrhub.jsx
@@ -1,79 +1,88 @@
-import React from 'react'
-import frame1 from '../../assets/Vector1.png'
-import { motion } from 'framer-motion'
+import React from "react";
+import frame1 from "../../assets/Vector1.png";
+import { motion } from "framer-motion";
 
 const Whystrhub = () => {
+  const descriptions = [
+    {
+      title: "Professional courses",
+      description:
+        "We offer well crafted Professional course that equip you with in-demand skills that are neded to advance your career and knowledge.",
+      image: frame1,
+    },
+    {
+      title: "Expert Instructors",
+      description:
+        "Learn from industry experts who bring real-world experience and insights to the classroom, ensuring you receive practical knowledge that can be applied immediately.",
+      image: frame1,
+    },
+    {
+      title: "Flexible Learning",
+      description:
+        "Our courses are designed to fit your schedule, allowing you to learn at your own pace and convenience, whether you prefer self-paced learning or live sessions.",
+      image: frame1,
+    },
+    {
+      title: "Community Support",
+      description:
+        "Join a vibrant community of learners and professionals where you can network, collaborate, and get support throughout your learning journey.",
+      image: frame1,
+    },
+    {
+      title: "Career Advancement",
+      description:
+        "Gain the skills and certifications needed to advance your career, open new job opportunities, and increase your earning potential in the tech industry.",
+      image: frame1,
+    },
+    {
+      title: "Affordable Pricing",
+      description:
+        "We offer competitive pricing and various payment options to make quality education accessible to everyone, without compromising on the quality of our courses.",
+      image: frame1,
+    },
+  ];
+
   return (
     <motion.section
-   initial={{ opacity: 0, y: 50 }}        // start hidden & pushed down
-  whileInView={{ opacity: 1, y: 0 }}     // animate when in view
-  transition={{ duration: 0.8, ease: "easeOut" }}
-  viewport={{ once: true, amount: 0.2 }}
-
-    className="flex items-center justify-center bg-[rgba(12,45,8,1)] w-full h-[750px] whychoose">
-        <div className="flex items-center flex-col w-full h-[] ">
-            <h1 className="text-[3.2rem] font-semibold text-center text-white mb-[30px] whychoosetext">Why choose StreetHub Academy</h1>
+      initial={{ opacity: 0, y: 50 }} // start hidden & pushed down
+      whileInView={{ opacity: 1, y: 0 }} // animate when in view
+      transition={{ duration: 0.8, ease: "easeOut" }}
+      viewport={{ once: true, amount: 0.2 }}
+      className="flex items-center justify-center bg-[rgba(12,45,8,1)] w-full  whychoose"
+    >
+      <div className="flex items-center flex-col w-full h-[] ">
+        <h1 className="text-[3.2rem] font-semibold text-center text-white mb-[30px] whychoosetext">
+          Why choose StreetHub Academy
+        </h1>
         <div className="flex flex-wrap items-center justify-center h-[90%] w-full bg-ed-500 gap-10">
-            <div className=" flex items-center justify-between pl-[10px] rounded-lg border-[1px] w-[380px] h-[280px] border-white">
-              <div className="flex items-top justify-center  h-[75%]"><img src={frame1} alt="" className=" w-[50px] h-[47px]" /></div>
+          {descriptions.map((description, index) => (
+            <div
+              key={index}
+              className=" flex items-center justify-between pl-[10px] rounded-lg border-[1px] w-[380px] h-[280px] border-white"
+            >
+              <div className="flex items-top justify-center  h-[75%]">
+                <img
+                  src={description.image}
+                  alt=""
+                  className=" w-[50px] h-[47px]"
+                />
+              </div>
               <div className="flex justify-between w-[78%]  h-[75%]  flex-col ">
-                <h1 className="text-2xl text-white font-bold">Professional courses</h1>
+                <h1 className="text-2xl text-white font-bold">
+                  {description.title}
+                </h1>
                 <div className=" w-[90%]">
-                  <p className="text-white text-l">We offer well crafted Professional course that equip you with in-demand skills that are neded to advance your career and knowledge.</p>
+                  <p className="text-white text-l">
+                    {description.description}
+                  </p>
                 </div>
               </div>
             </div>
-   <div className=" flex items-center justify-between pl-[10px] rounded-lg border-[1px] w-[380px] h-[280px] border-white">
-              <div className="flex items-top justify-center  h-[75%]"><img src={frame1} alt="" className=" w-[50px] h-[47px]" /></div>
-              <div className="flex justify-between w-[78%]  h-[75%]  flex-col ">
-                <h1 className="text-2xl text-white font-bold">Professional courses</h1>
-                <div className=" w-[90%]">
-                  <p className="text-white text-xl">We offer well crafted Professional course that equip you with in-demand skills that are neded to advance your career and knowledge.</p>
-                </div>
-              </div>
-            </div> 
-               <div className=" flex items-center justify-between pl-[10px] rounded-lg border-[1px] w-[380px] h-[280px] border-white">
-              <div className="flex items-top justify-center  h-[75%]"><img src={frame1} alt="" className=" w-[50px] h-[47px]" /></div>
-              <div className="flex justify-between w-[78%]  h-[75%]  flex-col ">
-                <h1 className="text-2xl text-white font-bold">Professional courses</h1>
-                <div className=" w-[90%]">
-                  <p className="text-white text-xl">We offer well crafted Professional course that equip you with in-demand skills that are neded to advance your career and knowledge.</p>
-                </div>
-              </div>
-            </div>       
-               <div className=" flex items-center justify-between pl-[10px] rounded-lg border-[1px] w-[380px] h-[280px] border-white">
-              <div className="flex items-top justify-center  h-[75%]"><img src={frame1} alt="" className=" w-[50px] h-[47px]" /></div>
-              <div className="flex justify-between w-[78%]  h-[75%]  flex-col ">
-                <h1 className="text-2xl text-white font-bold">Professional courses</h1>
-                <div className=" w-[90%]">
-                  <p className="text-white text-xl">We offer well crafted Professional course that equip you with in-demand skills that are neded to advance your career and knowledge.</p>
-                </div>
-              </div>
-            </div>
-               <div className=" flex items-center justify-between pl-[10px] rounded-lg border-[1px] w-[380px] h-[280px] border-white">
-              <div className="flex items-top justify-center  h-[75%]"><img src={frame1} alt="" className=" w-[50px] h-[47px]" /></div>
-              <div className="flex justify-between w-[78%]  h-[75%]  flex-col ">
-                <h1 className="text-2xl text-white font-bold">Professional courses</h1>
-                <div className=" w-[90%]">
-                  <p className="text-white text-xl">We offer well crafted Professional course that equip you with in-demand skills that are neded to advance your career and knowledge.</p>
-                </div>
-              </div>
-            </div>
-               <div className=" flex items-center justify-between pl-[10px] rounded-lg border-[1px] w-[380px] h-[280px] border-white">
-              <div className="flex items-top justify-center  h-[75%]"><img src={frame1} alt="" className=" w-[50px] h-[47px]" /></div>
-              <div className="flex justify-between w-[78%]  h-[75%]  flex-col ">
-                <h1 className="text-2xl text-white font-bold">Professional courses</h1>
-                <div className=" w-[90%]">
-                  <p className="text-white text-xl">We offer well crafted Professional course that equip you with in-demand skills that are neded to advance your career and knowledge.</p>
-                </div>
-              </div>
-            </div>
-            
-            
-            </div>
+          ))}
         </div>
+      </div>
     </motion.section>
-  )
-}
+  );
+};
 
-export default Whystrhub
+export default Whystrhub;
