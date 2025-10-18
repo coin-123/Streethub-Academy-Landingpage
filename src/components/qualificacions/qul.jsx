743d892e3c3a644fbf4172614377e88fbb1b14f7
coin-123 <isobednlemchukwu@gmail.com>
2025-10-18 11:33:30 +0100

    Refactor Qul component layout for improved structure and readability; enhance responsiveness and visual appeal

--- src/components/qualificacions/qul.jsx
+++ src/components/qualificacions/qul.jsx
@@ -1,20 +1,42 @@
-
 import React from 'react'
-import frame1 from '../../assets/5k+.png'
 import { useInView } from 'react-intersection-observer'
 
+const status = [
+  { head: '100+', para: 'Students since 2021' },
+  { head: '80%', para: 'Fully Employed' },
+  { head: '90%', para: 'Certified graduates' },
+]
 
 const Qul = () => {
-  const { ref, inView } = useInView({ triggerOnce: true, threshold: 0.2 })
+  const { ref, inView } = useInView({ triggerOnce: true, threshold: 0 })
+
   return (
-    <section className="flex items-center justify-center h-[300px] ">
-      <img
-        ref={ref}
-        src={frame1}
-        alt=""
-        className={`transition-opacity duration-2000 ${
-          inView ? 'opacity-100 animate-fade-in' : 'opacity-0'}`}
-      />
+    <section
+      ref={ref}
+      className={`flex items-center justify-center sm:h-[300px] transition-opacity duration-1000  ${
+        inView ? 'opacity-100' : 'opacity-0'
+      }`}
+    >
+      <div className="flex flex-col sm:flex-row gap-4 p-8 rounded-lg">
+        
+        {status.map((statu, index) => (
+
+          <div 
+          key={index} 
+          className="border-r border-[gray] text-white p-4 rounded-lg  ">
+
+            <p 
+            className="text-2xl font-bold text-black ">
+              {statu.head}
+              </p>
+
+            <p className='text-black'>
+              {statu.para}
+              </p>
+
+          </div>
+        ))}
+      </div>
     </section>
   )
 }
