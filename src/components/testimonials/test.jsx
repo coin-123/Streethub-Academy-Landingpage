605eae187f700221988b05c4ca56faf942458fbf
coin-123 <isobednlemchukwu@gmail.com>
2025-10-21 21:55:06 +0100

    Add react-icons dependency and update Navbar and Testimonials components for improved styling and functionality

--- src/components/testimonials/test.jsx
+++ src/components/testimonials/test.jsx
@@ -1,45 +1,54 @@
-import React from 'react';
-import frame1 from '../../assets/test1.png';
-import frame2 from '../../assets/image2.png';
-import frame3 from '../../assets/unsplash.png';
-import frame4 from '../../assets/Jessica.png';
-import frame5 from '../../assets/jesstext.png';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination, Keyboard, Navigation } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import { motion } from 'framer-motion';
+import React from "react";
+import frame1 from "../../assets/test1.png";
+import frame2 from "../../assets/image2.png";
+import frame3 from "../../assets/unsplash.png";
+import frame4 from "../../assets/Jessica.png";
+import frame5 from "../../assets/jesstext.png";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Keyboard, Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import { motion } from "framer-motion";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+import { useRef } from "react";
 
 const slidesData = [
-  { id: 1,
-    imgSrc: frame3, 
-    imgAlt: 'Description of frame3', 
-    description:"I went from a complete beginner to my first freelance project in a few months, the hands-on workshop gave me the confidence I needed to dive into my career. Streethub Academy transformed my tech jouurney!"
-   },
-
-  { id: 2,
-     imgSrc: frame3,
-      imgAlt: 'Description of frame3', 
-      description:"I went from a complete beginner to my first freelance project in a few months, the hands-on workshop gave me the confidence I needed to dive into my career. Streethub Academy transformed my tech jouurney!" 
-    },
-
-  { id: 3, 
+  {
+    id: 1,
     imgSrc: frame3,
-     imgAlt: 'Description of frame3',
-      description:"I went from a complete beginner to my first freelance project in a few months, the hands-on workshop gave me the confidence I needed to dive into my career. Streethub Academy transformed my tech jouurney!" 
-    },
+    imgAlt: "Description of frame3",
+    description:
+      "I went from a complete beginner to my first freelance project in a few months, the hands-on workshop gave me the confidence I needed to dive into my career. Streethub Academy transformed my tech jouurney!",
+  },
 
+  {
+    id: 2,
+    imgSrc: frame3,
+    imgAlt: "Description of frame3",
+    description:
+      "I went from a complete beginner to my first freelance project in a few months, the hands-on workshop gave me the confidence I needed to dive into my career. Streethub Academy transformed my tech jouurney!",
+  },
+
+  {
+    id: 3,
+    imgSrc: frame3,
+    imgAlt: "Description of frame3",
+    description:
+      "I went from a complete beginner to my first freelance project in a few months, the hands-on workshop gave me the confidence I needed to dive into my career. Streethub Academy transformed my tech jouurney!",
+  },
 ];
 
 const Test = () => {
-  return (
+  const swiperRef = useRef(null);
 
-    <motion.section 
-    initial={{ opacity: 0, y: 50 }}        // start hidden & pushed down
-  whileInView={{ opacity: 1, y: 0 }}     // animate when in view
-  transition={{ duration: 0.8, ease: "easeOut" }}
-  viewport={{ once: true, amount: 0.2 }}
-    className="flex justify-center w-full  sm:h-[680px] bg-amber-30 h-[430px] ">
+  return (
+    <motion.section
+      initial={{ opacity: 0, y: 50 }} // start hidden & pushed down
+      whileInView={{ opacity: 1, y: 0 }} // animate when in view
+      transition={{ duration: 0.8, ease: "easeOut" }}
+      viewport={{ once: true, amount: 0.2 }}
+      className="flex justify-center w-full  sm:h-[680px] bg-amber-30 h-[430px] "
+    >
       <div className="flex flex-col items-center sm:justify-between w-[80%] h-[590px]">
         {/* Top Images */}
         <div className="flex items-center justify-center w-[80%] h-[10%] relative">
@@ -53,25 +62,50 @@ const Test = () => {
 
         {/* Swiper motion.section */}
         <div className="flex items-center justify-center flex-col w-full h-[55%] sm:h-[85%] rounded-[20px] bg-[rgba(12,45,8,1)]">
+          <div className="sm:flex items-center justify-between p-4 relative left- top-[200px] w-[100%] hidden ">
+            <motion.div
+              whileHover={{ x: -5 }}
+              transition={{ type: "spring", stiffness: 400, damping: 1 }}
+              className=""
+              onClick={() => swiperRef.current?.slidePrev()}
+            >
+              <FaArrowLeft className="  text-[2rem] cursor-pointer text-white   transition duration-300  hover:text-[rgba(81,177,70,1)] " />
+            </motion.div>
+            <motion.div
+              whileHover={{ x: 5 }}
+              transition={{ type: "spring", stiffness: 400, damping: 1 }}
+              className=""
+              onClick={() => swiperRef.current?.slideNext()}
+            >
+              <FaArrowRight className="text-[2rem] cursor-pointer text-white transition duration-300  hover:text-[rgba(81,177,70,1)] " />
+            </motion.div>
+          </div>
           <Swiper
+            onSwiper={(swiper) => (swiperRef.current = swiper)}
             Pagination={{
               clickable: true,
-              dynamicBullets: true, 
+              dynamicBullets: true,
             }}
             Navigation={true}
             keyboard={{ enabled: true }}
             simulateTouch={true}
             // grabCursor={true}
-            modules={[Pagination, Keyboard,Navigation]}
+            modules={[Pagination, Keyboard, Navigation]}
             className="w-[60%] h-[80%]"
           >
             {slidesData.map((slide) => (
               <SwiperSlide key={slide.id}>
                 <div className="flex items-center justify-between flex-col w-full h-full">
-                  <img src={slide.imgSrc} alt={slide.imgAlt}  className='w-[55%] sm:w-[30%] ' />
+                  <img
+                    src={slide.imgSrc}
+                    alt={slide.imgAlt}
+                    className="w-[55%] sm:w-[30%] "
+                  />
                   <div className="flex items-center justify-between flex-col h-[180px]">
                     <img src={frame4} alt="Frame 4" />
-                    <p className="text-white text-center text-[0.6rem] sm:text-[96%]">{slide.description}</p>
+                    <p className="text-white text-center text-[0.6rem] sm:text-[96%]">
+                      {slide.description}
+                    </p>
                   </div>
                 </div>
               </SwiperSlide>
